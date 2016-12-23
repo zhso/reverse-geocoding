32fe0ea780746e83a007dc266c44abf7362df106
邵振华 <shaozh@ifeng.com>
2016-12-23 13:52:06 +0800

    release es5 edition. base on nodejs 4.7

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-/*global JSON*/
+/*global JSON,Buffer,require,exports*/
 'use strict';
-const http = require('http'),
+var http = require('http'),
     url = require('url'),
     querystring = require('querystring');
-exports.location = (config, callback) => {
+exports.location = function (config, callback) {
     if (!config || !callback) {
         throw new Error('Invalid arguments number.');
     } else if (!config.latitude || !config.longitude) {
         throw new Error('Latitude or Longitude not found.');
     }
-    //min the variable name:) node.js 5 does not support destructuring:(
-    let {
-        latitude,
-        longitude,
-        map
-    } = config;
-    //clean other params
+    var latitude = config.latitude,
+        longitude = config.longitude,
+        map = config.map;
     delete config.latitude;
     delete config.longitude;
-    let address = `http://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}`;
+    var address = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude;
     switch (map) {
     case 'baidu':
-        address = `http://api.map.baidu.com/geocoder/v2/?output=json&location=${latitude},${longitude}`;
+        address = 'http://api.map.baidu.com/geocoder/v2/?output=json&location=' + latitude + ',' + longitude;
         break;
     default:
         break;
     }
     delete config.map;
     //build interface address
-    address += `&${querystring.stringify(config)}`;
-    let options = config.options || url.parse(address);
+    address += '&' + querystring.stringify(config);
+    var options = config.options || url.parse(address);
     options.path = options.path || address;
-    new Promise((resolve, reject) => {
-        http.get(options, response => {
-            let bufferList = [];
-            response.on('data', chunk => {
+    try {
+        http.get(options, function (response) {
+            var bufferList = [];
+            response.on('data', function (chunk) {
                 bufferList.push(chunk);
-            }).on('end', () => {
-                let data = JSON.parse(Buffer.concat(bufferList).toString());
+            }).on('end', function () {
+                var data = JSON.parse(Buffer.concat(bufferList).toString());
                 //be ware, every interface return back data was not same.
                 if (data.status === 'OK' || data.status === 0) {
-                    resolve(data);
+                    callback(undefined, data);
                 } else {
-                    reject(data.status);
+                    callback(data.status);
                 }
-            }).on('error', error => {
-                reject(error);
+            }).on('error', function (error) {
+                callback(error);
             });
         });
-    }).then(data => {
-        callback(undefined, data);
-    }, error => {
-        callback(error);
-    }).catch(error => {
-        callback(error);
-    })
+    }catch(err){
+        callback(err);
+    }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,31 @@
 'use strict';
-const reverse = require('./../index.js');
-const chai = require('chai');
-const expect = chai.expect;
-describe(".location()", () => {
-    it("{arguments} number invalid should throw error.", ()=> {
+var reverse = require('./../index.js'),
+    chai = require('chai'),
+    expect = chai.expect;
+describe(".location()", function () {
+    it("{arguments} number invalid should throw error.", function () {
         expect(reverse.location).to.throw(Error);
     });
-    it('{latitude} or {longitude} undefined should throw error.', ()=> {
-        expect(()=> {
-            reverse.location({}, ()=> {
-            });
+    it('{latitude} or {longitude} undefined should throw error.', function () {
+        expect(function () {
+            reverse.location({}, function () {});
         }).to.throw(Error);
     });
-    it('valid gps should return location.', done=> {
+    it('valid gps should return location.', function (done) {
         reverse.location({
             'latitude': 40.00403611111111,
             'longitude': 116.48485555555555
-        }, (err, data)=> {
-            if (data) {
-                done();
-            }
+        }, function (err, data) {
+            data && done(); //
         });
     });
-    it('custom options should be valid.', done=> {
+    it('custom options should be valid.', function (done) {
         reverse.location({
             'latitude': 40.00403611111111,
             'longitude': 116.48485555555555,
             'language': 'zh-cn'
-        }, (err, data)=> {
-            if (data) {
-                done();
-            }
+        }, function (err, data) {
+            data && done();
         });
     });
     //because baidu maps need private token, so... pass the test;)
